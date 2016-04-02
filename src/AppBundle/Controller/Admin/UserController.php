0046f0b402e5dfd457bc526f6d6910f203605872
madangel <contact@bfoucher.fr>
2016-04-02 03:11:29 +0200

    Ban a User

--- src/AppBundle/Controller/Admin/UserController.php
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -24,6 +25,35 @@ class UserController extends Controller
 
         return $this->render('admin/user/list.html.twig',[
             'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route("/{user}", name="admin_users_edit")
+     */
+    public function editAction(Request $request,User $user)
+    {
+        return $this->render('admin/user/edit.html.twig',[
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/{user}/ban", name="admin_users_ban")
+     */
+    public function banAction(Request $request,User $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        if ($user->isEnabled()) {
+            $user->setEnabled(0);
+        }else{
+            $user->setEnabled(1);
+        }
+        $em->persist($user);
+        $em->flush();
+
+        return $this->render('admin/user/edit.html.twig',[
+            'user' => $user
         ]);
     }
 }
