d60803ec6ae38e3d2c92b14220c1bc8c2dd5fd35
baptiste <contact@bfoucher.fr>
2016-04-04 21:14:37 +0200

    Add Fixtures

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
+use AppBundle\Entity\Picture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\User;
@@ -60,19 +61,103 @@ class LoadUserData implements FixtureInterface
                 $serie->setThTvdbID(0);
                 $serie->setAuthor($userModerator);
                 $serie->setValidated(true);
-                //$serie->setPicture('https://commons.wikimedia.org/w/index.php?title=File:Girls-logo.svg&lang=fr&uselang=fr');
+                $seriePicture = new Picture();
+                $seriePicture->setUrl('https://commons.wikimedia.org/w/index.php?title=File:Girls-logo.svg&lang=fr&uselang=fr');
+                $seriePicture->setAlt('Affiche');
+                $seriePicture->setValidated(true);
+                $seriePicture->setSerie($serie);
+                $serie->addPicture($seriePicture);
                 $manager->persist($serie);
+                $manager->persist($seriePicture);
 
                 $episode = new Episode();
                 $episode->setName('Les aventures de José');
                 $episode->setDescription('José est poursuivit par Claudette nue avec son fusil afin de récupérer son bien à savoir : son dentier ');
-                $episode->setserie($serie);
+                $episode->setSerie($serie);
                 $episode->setSaison(1);
+                $episode->setEpisodeNumber(1);
                 $episode->setValidated(true);
                 $episode->setLanguage($french);
                 $episode->setAuthor($user);
 
                 $manager->persist($episode);
+
+                $serie = new Serie();
+                $serie->setName('Silicon Valley');
+                $serie->setDescription('Dans la Silicon Valley d\'aujourd\'hui, les personnes les plus qualifiées pour réussir ne sont pas forcément celles les plus qualifiées pour savoir comment le gérer...');
+                $serie->setLanguage($french);
+                $serie->setThTvdbID(0);
+                $serie->setAuthor($userAdmin);
+                $serie->setValidated(false);
+                $seriePicture = new Picture();
+                $seriePicture->setUrl('http://fr.web.img6.acsta.net/pictures/14/03/05/18/02/574226.jpg');
+                $seriePicture->setAlt('Affiche');
+                $seriePicture->setValidated(true);
+                $seriePicture->setSerie($serie);
+                $serie->addPicture($seriePicture);
+                $manager->persist($serie);
+                $manager->persist($seriePicture);
+
+                $episode = new Episode();
+                $episode->setName('Sand Hill Shuffle');
+                $episode->setDescription('Monica confronte son nouveau patron, tandis que Richard doit prendre une décision importante impliquant le futur de l\'entreprise.');
+                $episode->setSerie($serie);
+                $episode->setSaison(2);
+                $episode->setEpisodeNumber(1);
+                $episode->setValidated(false);
+                $episode->setLanguage($french);
+                $episode->setAuthor($user);
+
+                $manager->persist($episode);
+
+                $episode = new Episode();
+                $episode->setName('Runaway Devaluation');
+                $episode->setDescription('Au lendemain de l\'annonce de Hooli, Richard et ses amis cherchent un soutien financier. Dinesh cherche à mettre un terme à la campagne Kickstarter de son cousin.');
+                $episode->setSerie($serie);
+                $episode->setSaison(2);
+                $episode->setEpisodeNumber(2);
+                $episode->setValidated(false);
+                $episode->setLanguage($french);
+                $episode->setAuthor($user);
+
+                $manager->persist($episode);
+
+                $episode = new Episode();
+                $episode->setName('Produit Minimum Viable');
+                $episode->setDescription('Dans la Silicon Valley, où naissent les entreprises les plus prospères de l\'ère informatique, six programmeurs partagent la même maison et la même ambition : se faire une place dans cet univers. Ils découvrent qu\'être hautement qualifié dans un domaine ne donne pas l\'assurance de la réussite...');
+                $episode->setSerie($serie);
+                $episode->setSaison(1);
+                $episode->setEpisodeNumber(1);
+                $episode->setValidated(false);
+                $episode->setLanguage($french);
+                $episode->setAuthor($user);
+
+                $manager->persist($episode);
+
+                $episode = new Episode();
+                $episode->setName('La table de capitalisation');
+                $episode->setDescription('Après une fête au Hacker Hostel, Richard et Erlich apprennent que Peter Gregory ne les paiera que lorsqu\'ils auront produit un plan de développement faisant état d\'une équipe reduite. Désespéré, Richard engage Jared, un ancien sous-fifre de Belson...');
+                $episode->setSerie($serie);
+                $episode->setSaison(1);
+                $episode->setEpisodeNumber(2);
+                $episode->setValidated(false);
+                $episode->setLanguage($french);
+                $episode->setAuthor($user);
+
+                $manager->persist($episode);
+
+                $episode = new Episode();
+                $episode->setName('Statuts légaux');
+                $episode->setDescription('Richard découvre que le nom «Pied Piper» a déjà été déposé. Erlich se triture donc les méninges à la recherche d\'un nouveau nom pour la société. L\'attitude de Peter Gregory devient difficile à comprendre...');
+                $episode->setSerie($serie);
+                $episode->setSaison(1);
+                $episode->setEpisodeNumber(3);
+                $episode->setValidated(false);
+                $episode->setLanguage($french);
+                $episode->setAuthor($user);
+
+                $manager->persist($episode);
+
 
 
                 $manager->flush();
